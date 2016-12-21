7a38ce6cc079d5158902c734c4d7e06b33a7cc65
stepping <michael.stepping@hof-university.de>
2016-12-21 10:39:18 +0100

    removed dead code

--- app/src/main/java/de/hof/university/app/model/meal/Meal.java
+++ app/src/main/java/de/hof/university/app/model/meal/Meal.java
@@ -24,168 +24,111 @@ import java.util.Date;
 /**
  * Created by Lukas on 25.11.2015.
  */
-public class Meal implements Serializable{
-    private String name;
-    private String price;
-    private final String weekDay;
-    private final String category;
-    private String tariff;
-    private final Date day;
-    final private ArrayList<Integer> attributes;
-
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public Meal(final Meal meal) {
-//        name= meal.name;
-//        price = meal.price;
-//        weekDay = meal.weekDay;
-//        category = meal.category;
-//        tariff = meal.tariff;
-//        day= meal.day;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
+public class Meal implements Serializable {
+	private String name;
+	private String price;
+	private final String weekDay;
+	//TODO was ist eine Kategorie?
+	private final String category;
+	private String tariff;
+	private final Date day;
+	// TODO welche Attribute sind hier gemeint?
+	final private ArrayList<Integer> attributes;
 
 
-    // public Meal(String name, String price) {
-    //    this.name = name;
-    //    this.price = price;
-    //}
+	public Meal(final Date tag, final String weeekDay, final String category, final String name) {
+		this.weekDay = weeekDay;
+		this.day = tag;
+		this.category = category;
+		this.name = name.replace("\\", "");
+		this.attributes = new ArrayList<>();
+	}
 
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    protected Meal(final Parcel in) {
-//        name = in.readString();
-//        price = in.readString();
-//        weekDay = in.readString();
-//        category = in.readString();
-//        tariff = in.readString();
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
 
-    public Meal(final Date tag, final String weeekDay, final String category, final String name) {
-        this.weekDay = weeekDay;
-        this.day = tag;
-        this.category = category;
-        this.name = name.replace("\\", "");
-        this.attributes = new ArrayList<>();
-    }
+	public final Date getDay() {
+		return day;
+	}
 
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public Meal(final String name, final String preis, final String weekDay, final String kategorie,
-//                final String tarif, final Date day) {
-//        this.name = name;
-//        setPrice(preis);
-//        this.weekDay = weekDay;
-//        this.category = kategorie;
-//        this.tariff = tarif;
-//        this.day = day;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
+	public final void setName(String name) {
+		this.name = name;
+	}
 
-    public final Date getDay() {
-        return day;
-    }
+	public final void setPrice(String price) {
+		this.price = price;
+	}
 
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public void setDay(Date day) {
-//        this.day = day;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
+	public final String getWeekDay() {
+		return weekDay;
+	}
 
-    public final void setName(String name) {
-        this.name = name;
-    }
+	public final String getCategory() {
+		return category;
+	}
 
-    public final void setPrice(String price) {
-        this.price = price;
-    }
+	public final void setTariff(String tariff) {
+		this.tariff = tariff;
+	}
 
-    public final String getWeekDay() {
-        return weekDay;
-    }
+	public final String getName() {
+		String result;
+		result = name;
+		String tmpAttributes = "";
+		for ( int a : attributes
+				) {
+			if ( !tmpAttributes.isEmpty() ) {
+				tmpAttributes += ", ";
+			}
+			if ( a == 1 ) {                          // TODO Schöner machen nicht hardcoded
+				tmpAttributes += "hausgemacht";
+			} else if ( a == 2 ) {
+				tmpAttributes += "Wild";
+			} else if ( a == 3 ) {
+				tmpAttributes += "Geflügel";
+			} else if ( a == 4 ) {
+				tmpAttributes += "regional";
+			} else if ( a == 5 ) {
+				tmpAttributes += "Schwein";
+			} else if ( a == 6 ) {
+				tmpAttributes += "Fisch";
+			} else if ( a == 7 ) {
+				tmpAttributes += "vegetarisch";
+			} else if ( a == 8 ) {
+				tmpAttributes += "Rind";
+			} else if ( a == 9 ) {
+				tmpAttributes += "nachhaltiger Fang";
+			} else if ( a == 10 ) {
+				tmpAttributes += "vegan";
+			} else if ( a == 11 ) {
+				tmpAttributes += "Lamm";
+			} else if ( a == 12 ) {
+				tmpAttributes += "Meeresfrüchte";
+			} else if ( a == 13 ) {
+				tmpAttributes += "Mensa Vital";
+			} else if ( a == 14 ) {
+				tmpAttributes += "nicht vegetarisch";
+			} else if ( a == 15 ) {
+				tmpAttributes += "Kräuterküche";
+			}
+		}
+		if ( !tmpAttributes.isEmpty() ) {
+			result += " (" + tmpAttributes + ")";
+		}
+		return result;
+	}
 
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public void setWeekDay(String weeekDay) {
-//        this.weekDay = weeekDay;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
+	//TODO stimmt das? Kann man doch weglassen?
+	public final String getPrice() {
+		try {
+			final Double number = Double.valueOf(price);
+			DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+			return decimalFormat.format(number);
+		} catch ( final NumberFormatException nfe ) {
+			return price;
+		}
+	}
 
-    public final String getCategory() {
-        return category;
-    }
+	public void addAttribute(final String xmlText) {
 
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public void setCategory(String category) {
-//        this.category = category;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
-
-// --Commented out by Inspection START (17.07.2016 20:11):
-//    public String getTariff() {
-//        return tariff;
-//    }
-// --Commented out by Inspection STOP (17.07.2016 20:11)
-
-    public final void setTariff(String tariff) {
-        this.tariff = tariff;
-    }
-
-    public final String getName() {
-        String result;
-        result = name;
-        String tmpAttributes = "";
-        for (int a : attributes
-                ) {
-            if (tmpAttributes != "") {
-                tmpAttributes += ", ";
-            }
-            if (a == 1) {                          // TODO Schöner machen nicht hardcoded
-                tmpAttributes += "hausgemacht";
-            } else if (a == 2) {
-                tmpAttributes += "Wild";
-            } else if (a == 3) {
-                tmpAttributes += "Geflügel";
-            } else if (a == 4) {
-                tmpAttributes += "regional";
-            } else if (a == 5) {
-                tmpAttributes += "Schwein";
-            } else if (a == 6) {
-                tmpAttributes += "Fisch";
-            } else if (a == 7) {
-                tmpAttributes += "vegetarisch";
-            } else if (a == 8) {
-                tmpAttributes += "Rind";
-            } else if (a == 9) {
-                tmpAttributes += "nachhaltiger Fang";
-            } else if (a == 10) {
-                tmpAttributes += "vegan";
-            } else if (a == 11) {
-                tmpAttributes += "Lamm";
-            } else if (a == 12) {
-                tmpAttributes += "Meeresfrüchte";
-            } else if (a == 13) {
-                tmpAttributes += "Mensa Vital";
-            } else if (a == 14) {
-                tmpAttributes += "nicht vegetarisch";
-            } else if (a == 15) {
-                tmpAttributes += "Kräuterküche";
-            }
-        }
-        if (tmpAttributes != "") {
-            result += " (" + tmpAttributes + ")";
-        }
-        return result;
-    }
-
-    public final String getPrice() {
-        try {
-            final Double number = Double.valueOf(price);
-            DecimalFormat decimalFormat = new DecimalFormat("#0.00");
-            return decimalFormat.format(number);
-        } catch (final NumberFormatException nfe) {
-            return price;
-        }
-    }
-
-    public void addAttribute(String xmlText) {
-        attributes.add(Integer.parseInt(xmlText));
-    }
+		attributes.add(Integer.parseInt(xmlText));
+	}
 }
