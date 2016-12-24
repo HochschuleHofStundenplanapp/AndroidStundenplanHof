7e04dbc6a688d31218d3cac93b9720904c29e450
stepping <michael.stepping@hof-university.de>
2016-12-24 12:11:46 +0100

    shift all String Utility methods to here

---
+++ app/src/main/java/de/hof/university/app/Util/MyString.java
@@ -0,0 +1,13 @@
+package de.hof.university.app.Util;
+
+/**
+ * Created by stepping on 24.12.2016.
+ */
+
+public final class MyString {
+
+
+	public static String URLReplaceWhitespace(final String str) {
+		return str.replace(" ", "%20");
+	}
+}
