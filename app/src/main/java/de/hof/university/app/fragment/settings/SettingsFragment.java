f7548dd0de2107b22926aaaa8e5d0ec859afa6ee
Daniel Glaser <danielglaser9@gmail.com>
2017-03-21 22:47:08 +0100

    Push-Notifications aus Experimentellen Features raus genommen.

--- app/src/main/java/de/hof/university/app/fragment/settings/SettingsFragment.java
+++ app/src/main/java/de/hof/university/app/fragment/settings/SettingsFragment.java
@@ -142,17 +142,17 @@ public class SettingsFragment extends PreferenceFragment {
 
 		if ( experimentalFeatures.isChecked() ) {
 			edtLogin.setEnabled(true);
-			changes_notifications.setEnabled(true);
+			//changes_notifications.setEnabled(true);
 		} else {
 			edtLogin.setEnabled(false);
-			changes_notifications.setEnabled(false);
+			//changes_notifications.setEnabled(false);
 		}
 
 		experimentalFeatures.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference, Object newValue) {
 				final Preference edtLogin = findPreference("login");
-				final CheckBoxPreference changes_notifications = (CheckBoxPreference) findPreference("changes_notifications");
+//				final CheckBoxPreference changes_notifications = (CheckBoxPreference) findPreference("changes_notifications");
 				final MainActivity activity = (MainActivity) getActivity();
 				if ( (Boolean) newValue ) {
 					new AlertDialog.Builder(getView().getContext())
@@ -167,7 +167,7 @@ public class SettingsFragment extends PreferenceFragment {
 							.setIcon(android.R.drawable.ic_dialog_alert)
 							.show();
 					edtLogin.setEnabled(true);
-					if (changes_notifications != null) {
+					/*if (changes_notifications != null) {
 						changes_notifications.setEnabled(true);
 						// falls ausgewählt war
 						if (changes_notifications.isChecked()) {
@@ -175,16 +175,16 @@ public class SettingsFragment extends PreferenceFragment {
 							// falls schon ein Stundenplan angelegt wurde
 							DataManager.getInstance().registerFCMServerForce(MainActivity.contextOfApplication);
 						}
-					}
+					}*/
 					activity.displayExperimentalFeaturesMenuEntries(true);
 
 				} else {
 					edtLogin.setEnabled(false);
-					if (changes_notifications != null) {
+					/*if (changes_notifications != null) {
 						changes_notifications.setEnabled(false);
 						// von Push-Notifications abmelden
 						new RegisterLectures().deRegisterLectures();
-					}
+					}*/
 					activity.displayExperimentalFeaturesMenuEntries(false);
 				}
 				return true;
