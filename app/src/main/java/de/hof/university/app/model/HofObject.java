dae95682742af294e0f2926c153afc323f9d76cd
stepping <michael.stepping@hof-university.de>
2017-05-12 21:56:18 +0200

    removed not used saveObject and readObject ....

--- app/src/main/java/de/hof/university/app/model/HofObject.java
+++ app/src/main/java/de/hof/university/app/model/HofObject.java
@@ -37,13 +37,9 @@ import de.hof.university.app.Util.Log;
 public class HofObject implements Serializable {
 	public final static String TAG = "SaveObject";
 
-	private final static String filename = "SaveObject";
-	private static final long serialVersionUID = Define.serialVersionUID;
-
-	private Date lastSaved;
+	private Date lastSaved = null;
 
 	public HofObject() {
-		this.lastSaved = null;
 	}
 
 	public Date getLastSaved() {
@@ -54,36 +50,4 @@ public class HofObject implements Serializable {
 		this.lastSaved = lastSaved;
 	}
 
-	// this is the general method to serialize an object
-	//
-	private void saveObject(final Context context) {
-		try {
-			final File file = new File(context.getFilesDir(), this.filename);
-			final FileOutputStream fos = new FileOutputStream(file);
-			final ObjectOutputStream os = new ObjectOutputStream(fos);
-			os.writeObject(this);
-			os.close();
-			fos.close();
-		} catch ( final IOException e ) {
-			Log.e( TAG, "Fehler beim Speichern des Objektes", e );
-		}
-	}
-
-	// this is the general method to serialize an object
-	private Object readObject(final Context context) {
-		Object result = null;
-		try {
-			final File file = new File(context.getFilesDir(), this.filename);
-			if ( file.exists() ) {
-				final FileInputStream fis = new FileInputStream(file);
-				final ObjectInputStream is = new ObjectInputStream(fis);
-				result = is.readObject();
-				is.close();
-				fis.close();
-			}
-		} catch ( final Exception e ) {
-			Log.e( TAG, "Fehler beim Einlesen", e );
-		}
-		return result;
-	}
 }
