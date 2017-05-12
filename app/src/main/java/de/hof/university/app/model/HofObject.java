d4e25826b45b59a347b2069b799f16d7281d4572
stepping <michael.stepping@hof-university.de>
2017-05-12 22:31:46 +0200

    optimized imports

--- app/src/main/java/de/hof/university/app/model/HofObject.java
+++ app/src/main/java/de/hof/university/app/model/HofObject.java
@@ -16,19 +16,8 @@
 
 package de.hof.university.app.model;
 
-import android.content.Context;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Date;
-
-import de.hof.university.app.Util.Define;
-import de.hof.university.app.Util.Log;
 
 /**
  * Created by Daniel on 01.12.2016.
