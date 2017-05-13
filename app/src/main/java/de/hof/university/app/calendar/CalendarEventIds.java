1413c945edfe44010964803e297eb670d3ef9f2c
Daniel Glaser <danielglaser9@gmail.com>
2017-05-13 14:40:08 +0200

    Noch eine Klasse zum Speichern der IDs

---
+++ app/src/main/java/de/hof/university/app/calendar/CalendarEventIds.java
@@ -0,0 +1,40 @@
+package de.hof.university.app.calendar;
+
+import de.hof.university.app.Util.Define;
+import de.hof.university.app.model.HofObject;
+
+import java.io.InterruptedIOException;
+import java.io.Serializable;
+import java.util.Dictionary;
+import java.util.HashMap;
+
+/**
+ * Created by Daniel on 13.05.2017.
+ */
+
+public class CalendarEventIds extends HofObject implements Serializable {
+    private static final long serialVersionUID = Define.serialVersionUID;
+    private HashMap<String, String> lectureEventIDs;
+    private HashMap<String, String> changesEventIDs;
+
+    public CalendarEventIds() {
+        lectureEventIDs = new HashMap<>();
+        changesEventIDs = new HashMap<>();
+    }
+
+    public HashMap<String, String> getLectureEventIDs() {
+        return lectureEventIDs;
+    }
+
+    public HashMap<String, String> getChangesEventIDs() {
+        return changesEventIDs;
+    }
+
+    public void setLectureEventIDs(HashMap<String, String> lectureEventIDs) {
+        this.lectureEventIDs = lectureEventIDs;
+    }
+
+    public void setChangesEventIDs(HashMap<String, String> changesEventIDs) {
+        this.changesEventIDs = changesEventIDs;
+    }
+}
