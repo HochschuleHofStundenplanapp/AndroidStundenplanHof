308452564de7b671a539e7012325c0fcc9dba6a2
Daniel Glaser <danielglaser9@gmail.com>
2017-05-13 14:23:04 +0200

    Klassen für die Calendar Schnittstelle angelegt.

---
+++ app/src/main/java/de/hof/university/app/calendar/CalendarInterface.java
@@ -0,0 +1,38 @@
+package de.hof.university.app.calendar;
+
+/**
+ * Created by Daniel on 13.05.2017.
+ */
+
+public class CalendarInterface {
+
+    private static final CalendarInterface calendarInterface = new CalendarInterface();
+
+    public static CalendarInterface getInstance() {
+        return CalendarInterface.calendarInterface;
+    }
+
+    public CalendarInterface() {
+        // TODO
+    }
+
+    public void createCalendar() {
+        // TODO
+    }
+
+    public void removeCalendar() {
+        // TODO
+    }
+
+    public void createEvent() {
+        // TODO
+    }
+
+    public void updateEvent() {
+        // TODO
+    }
+
+    public void deleteEvent() {
+        // TODO
+    }
+}
---
+++ app/src/main/java/de/hof/university/app/calendar/CalendarInterfaceController.java
@@ -0,0 +1,24 @@
+package de.hof.university.app.calendar;
+
+/**
+ * Created by Daniel on 13.05.2017.
+ */
+
+public class CalendarInterfaceController {
+
+    public CalendarInterfaceController() {
+        // TODO
+    }
+
+    public void createAllEvents() {
+        // TODO
+    }
+
+    public void updateAllEvents() {
+        // TODO
+    }
+
+    public void removeAllEvents() {
+        // TODO
+    }
+}
