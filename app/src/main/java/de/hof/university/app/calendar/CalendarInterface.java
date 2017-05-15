1489f48f83352f65a1c976d2099e4ba6bc9ea330
Daniel Glaser <danielglaser9@gmail.com>
2017-05-15 11:28:55 +0200

    Methode zum Events erzeugen gefüllt mit Beispiel von Android Developer.

--- app/src/main/java/de/hof/university/app/calendar/CalendarInterface.java
+++ app/src/main/java/de/hof/university/app/calendar/CalendarInterface.java
@@ -1,5 +1,16 @@
 package de.hof.university.app.calendar;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.CalendarContract.Events;
+import android.support.v4.app.ActivityCompat;
+
+import java.util.Calendar;
+
 /**
  * Created by Daniel on 13.05.2017.
  */
@@ -8,12 +19,15 @@ public class CalendarInterface {
 
     private static final CalendarInterface calendarInterface = new CalendarInterface();
 
+    private long calID = 3;
+
     public static CalendarInterface getInstance() {
         return CalendarInterface.calendarInterface;
     }
 
     public CalendarInterface() {
         // TODO
+
     }
 
     public void createCalendar() {
@@ -24,8 +38,45 @@ public class CalendarInterface {
         // TODO
     }
 
-    public void createEvent() {
+    public void createEvent(Context context) {
         // TODO
+
+        long startMillis = 0;
+        long endMillis = 0;
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2012, 9, 14, 7, 30);
+        startMillis = beginTime.getTimeInMillis();
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2012, 9, 14, 8, 45);
+        endMillis = endTime.getTimeInMillis();
+
+
+        ContentResolver cr = context.getContentResolver();
+        ContentValues values = new ContentValues();
+        values.put(Events.DTSTART, startMillis);
+        values.put(Events.DTEND, endMillis);
+        values.put(Events.TITLE, "Jazzercise");
+        values.put(Events.DESCRIPTION, "Group workout");
+        values.put(Events.CALENDAR_ID, calID);
+        values.put(Events.EVENT_TIMEZONE, "America/Los_Angeles");
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Uri uri = cr.insert(Events.CONTENT_URI, values);
+
+        // get the event ID that is the last element in the Uri
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+        //
+        // ... do something with event ID
+        //
+        //
     }
 
     public void updateEvent() {
