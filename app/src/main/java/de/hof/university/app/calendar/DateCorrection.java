1a1618a6fd6a967d9c01df5a54d7bb81cb5a3f26
Daniel Glaser <danielglaser9@gmail.com>
2017-09-10 17:47:15 +0200

    Calendar mit GregorianCalendar ausgetauscht.

--- app/src/main/java/de/hof/university/app/calendar/DateCorrection.java
+++ app/src/main/java/de/hof/university/app/calendar/DateCorrection.java
@@ -2,6 +2,7 @@ package de.hof.university.app.calendar;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Daniel on 19.07.2017.
@@ -12,14 +13,14 @@ public class DateCorrection {
     private static DateCorrection dateCorrection = null;
 
     int summerStartDay = 15;
-    int summerStartMonth = Calendar.MARCH;
+    int summerStartMonth = GregorianCalendar.MARCH;
     int winterStartDay = 1;
-    int winterStartMonth = Calendar.OCTOBER;
+    int winterStartMonth = GregorianCalendar.OCTOBER;
 
     int summerEndDay = 10;
-    int summerEndMonth = Calendar.JULY;
+    int summerEndMonth = GregorianCalendar.JULY;
     int winterEndDay = 25;
-    int winterEndMonth = Calendar.JANUARY;
+    int winterEndMonth = GregorianCalendar.JANUARY;
 
     private Date lastYearSummerStartDate = new Date();
     private Date lastYearSummerEndDate = new Date();
@@ -41,30 +42,30 @@ public class DateCorrection {
     }
 
     private DateCorrection() {
-        Calendar currentDateCal = Calendar.getInstance();
+        Calendar currentDateCal = GregorianCalendar.getInstance();
         currentDateCal.setTime(new Date());
 
-        lastYearSummerStartDate = getSemesterStartDate(currentDateCal.get(Calendar.YEAR) - 1, "SS");
-        lastYearSummerEndDate = getSemesterEndDate(currentDateCal.get(Calendar.YEAR) - 1, "SS");
-        lastYearWinterStartDate = getSemesterStartDate(currentDateCal.get(Calendar.YEAR) - 1, "WS");
+        lastYearSummerStartDate = getSemesterStartDate(currentDateCal.get(GregorianCalendar.YEAR) - 1, "SS");
+        lastYearSummerEndDate = getSemesterEndDate(currentDateCal.get(GregorianCalendar.YEAR) - 1, "SS");
+        lastYearWinterStartDate = getSemesterStartDate(currentDateCal.get(GregorianCalendar.YEAR) - 1, "WS");
 
-        thisYearWinterEndDate = getSemesterEndDate(currentDateCal.get(Calendar.YEAR), "WS");
-        thisYearSummerStartDate = getSemesterStartDate(currentDateCal.get(Calendar.YEAR), "SS");
-        thisYearSummerEndDate = getSemesterEndDate(currentDateCal.get(Calendar.YEAR), "SS");
-        thisYearWinterStartDate = getSemesterStartDate(currentDateCal.get(Calendar.YEAR), "WS");
+        thisYearWinterEndDate = getSemesterEndDate(currentDateCal.get(GregorianCalendar.YEAR), "WS");
+        thisYearSummerStartDate = getSemesterStartDate(currentDateCal.get(GregorianCalendar.YEAR), "SS");
+        thisYearSummerEndDate = getSemesterEndDate(currentDateCal.get(GregorianCalendar.YEAR), "SS");
+        thisYearWinterStartDate = getSemesterStartDate(currentDateCal.get(GregorianCalendar.YEAR), "WS");
 
-        nextYearWinterEndDate = getSemesterEndDate(currentDateCal.get(Calendar.YEAR) + 1, "WS");
+        nextYearWinterEndDate = getSemesterEndDate(currentDateCal.get(GregorianCalendar.YEAR) + 1, "WS");
 
-        Calendar beginOfYearCalendar = Calendar.getInstance();
+        Calendar beginOfYearCalendar = GregorianCalendar.getInstance();
         beginOfYearCalendar.setTime(new Date());
-        beginOfYearCalendar.set(Calendar.MONTH, Calendar.JANUARY);
-        beginOfYearCalendar.set(Calendar.DATE, 1);
+        beginOfYearCalendar.set(GregorianCalendar.MONTH, GregorianCalendar.JANUARY);
+        beginOfYearCalendar.set(GregorianCalendar.DATE, 1);
         beginOfThisYearDate = beginOfYearCalendar.getTime();
 
-        Calendar endOfYearCalendar = Calendar.getInstance();
+        Calendar endOfYearCalendar = GregorianCalendar.getInstance();
         endOfYearCalendar.setTime(new Date());
-        endOfYearCalendar.set(Calendar.MONTH, Calendar.DECEMBER);
-        endOfYearCalendar.set(Calendar.DATE, 31);
+        endOfYearCalendar.set(GregorianCalendar.MONTH, GregorianCalendar.DECEMBER);
+        endOfYearCalendar.set(GregorianCalendar.DATE, 31);
         endOfThisYearDate = endOfYearCalendar.getTime();
     }
 
@@ -72,43 +73,43 @@ public class DateCorrection {
         Date correctStartDate = new Date();
 
         if (semester.equals("SS")) {
-            Calendar summerStartDateCalendar = Calendar.getInstance();
-            summerStartDateCalendar.set(Calendar.YEAR, year);
-            summerStartDateCalendar.set(Calendar.MONTH, summerStartMonth);
-            summerStartDateCalendar.set(Calendar.DATE, summerStartDay);
+            Calendar summerStartDateCalendar = GregorianCalendar.getInstance();
+            summerStartDateCalendar.set(GregorianCalendar.YEAR, year);
+            summerStartDateCalendar.set(GregorianCalendar.MONTH, summerStartMonth);
+            summerStartDateCalendar.set(GregorianCalendar.DATE, summerStartDay);
 
             // Wenn der 15.3 ein Freitag/Samstag/Sonntag ist beginnt der Vorlesungszeitraum am nächstfolgenden Montag
 
-            switch (summerStartDateCalendar.get(Calendar.DAY_OF_WEEK)) {
-                case Calendar.FRIDAY:
-                    summerStartDateCalendar.add(Calendar.DATE, 3);
+            switch (summerStartDateCalendar.get(GregorianCalendar.DAY_OF_WEEK)) {
+                case GregorianCalendar.FRIDAY:
+                    summerStartDateCalendar.add(GregorianCalendar.DATE, 3);
                     break;
-                case Calendar.SATURDAY:
-                    summerStartDateCalendar.add(Calendar.DATE, 2);
+                case GregorianCalendar.SATURDAY:
+                    summerStartDateCalendar.add(GregorianCalendar.DATE, 2);
                     break;
-                case Calendar.SUNDAY:
-                    summerStartDateCalendar.add(Calendar.DATE, 1);
+                case GregorianCalendar.SUNDAY:
+                    summerStartDateCalendar.add(GregorianCalendar.DATE, 1);
                     break;
             }
 
             correctStartDate = summerStartDateCalendar.getTime();
         } else if (semester.equals("WS")) {
-            Calendar winterStartDateCalendar = Calendar.getInstance();
-            winterStartDateCalendar.set(Calendar.YEAR, year);
-            winterStartDateCalendar.set(Calendar.MONTH, winterStartMonth);
-            winterStartDateCalendar.set(Calendar.DATE, winterStartDay);
+            Calendar winterStartDateCalendar = GregorianCalendar.getInstance();
+            winterStartDateCalendar.set(GregorianCalendar.YEAR, year);
+            winterStartDateCalendar.set(GregorianCalendar.MONTH, winterStartMonth);
+            winterStartDateCalendar.set(GregorianCalendar.DATE, winterStartDay);
 
             // Wenn der 1.10 ein Freitag/Samstag/Sonntag ist beginnt der Vorlesungszeitraum am nächstfolgenden Montag
 
-            switch (winterStartDateCalendar.get(Calendar.DAY_OF_WEEK)) {
-                case Calendar.FRIDAY:
-                    winterStartDateCalendar.add(Calendar.DATE, 3);
+            switch (winterStartDateCalendar.get(GregorianCalendar.DAY_OF_WEEK)) {
+                case GregorianCalendar.FRIDAY:
+                    winterStartDateCalendar.add(GregorianCalendar.DATE, 3);
                     break;
-                case Calendar.SATURDAY:
-                    winterStartDateCalendar.add(Calendar.DATE, 2);
+                case GregorianCalendar.SATURDAY:
+                    winterStartDateCalendar.add(GregorianCalendar.DATE, 2);
                     break;
-                case Calendar.SUNDAY:
-                    winterStartDateCalendar.add(Calendar.DATE, 1);
+                case GregorianCalendar.SUNDAY:
+                    winterStartDateCalendar.add(GregorianCalendar.DATE, 1);
                     break;
             }
 
@@ -122,43 +123,43 @@ public class DateCorrection {
         Date correctEndDate = new Date();
 
         if (semester.equals("SS")) {
-            Calendar summerEndDateCalendar = Calendar.getInstance();
-            summerEndDateCalendar.set(Calendar.YEAR, year);
-            summerEndDateCalendar.set(Calendar.MONTH, summerEndMonth);
-            summerEndDateCalendar.set(Calendar.DATE, summerEndDay);
+            Calendar summerEndDateCalendar = GregorianCalendar.getInstance();
+            summerEndDateCalendar.set(GregorianCalendar.YEAR, year);
+            summerEndDateCalendar.set(GregorianCalendar.MONTH, summerEndMonth);
+            summerEndDateCalendar.set(GregorianCalendar.DATE, summerEndDay);
 
             // Wenn der 10.7 ein Samstag/Sonntag/Montag ist endet der Vorlesungszeitraum am vorausgehenden Freitag
 
-            switch (summerEndDateCalendar.get(Calendar.DAY_OF_WEEK)) {
-                case Calendar.SATURDAY:
-                    summerEndDateCalendar.add(Calendar.DATE, -1);
+            switch (summerEndDateCalendar.get(GregorianCalendar.DAY_OF_WEEK)) {
+                case GregorianCalendar.SATURDAY:
+                    summerEndDateCalendar.add(GregorianCalendar.DATE, -1);
                     break;
-                case Calendar.SUNDAY:
-                    summerEndDateCalendar.add(Calendar.DATE, -2);
+                case GregorianCalendar.SUNDAY:
+                    summerEndDateCalendar.add(GregorianCalendar.DATE, -2);
                     break;
-                case Calendar.MONDAY:
-                    summerEndDateCalendar.add(Calendar.DATE, -3);
+                case GregorianCalendar.MONDAY:
+                    summerEndDateCalendar.add(GregorianCalendar.DATE, -3);
                     break;
             }
 
             correctEndDate = summerEndDateCalendar.getTime();
         } else if (semester.equals("WS")) {
-            Calendar winterEndDateCalendar = Calendar.getInstance();
-            winterEndDateCalendar.set(Calendar.YEAR, year);
-            winterEndDateCalendar.set(Calendar.MONTH, winterEndMonth);
-            winterEndDateCalendar.set(Calendar.DATE, winterEndDay);
+            Calendar winterEndDateCalendar = GregorianCalendar.getInstance();
+            winterEndDateCalendar.set(GregorianCalendar.YEAR, year);
+            winterEndDateCalendar.set(GregorianCalendar.MONTH, winterEndMonth);
+            winterEndDateCalendar.set(GregorianCalendar.DATE, winterEndDay);
 
             // Wenn der 25.1 ein Samstag/Sonntag/Montag ist endet der Vorlesungszeitraum am vorausgehenden Freitag
 
-            switch (winterEndDateCalendar.get(Calendar.DAY_OF_WEEK)) {
-                case Calendar.SATURDAY:
-                    winterEndDateCalendar.add(Calendar.DATE, -1);
+            switch (winterEndDateCalendar.get(GregorianCalendar.DAY_OF_WEEK)) {
+                case GregorianCalendar.SATURDAY:
+                    winterEndDateCalendar.add(GregorianCalendar.DATE, -1);
                     break;
-                case Calendar.SUNDAY:
-                    winterEndDateCalendar.add(Calendar.DATE, -2);
+                case GregorianCalendar.SUNDAY:
+                    winterEndDateCalendar.add(GregorianCalendar.DATE, -2);
                     break;
-                case Calendar.MONDAY:
-                    winterEndDateCalendar.add(Calendar.DATE, -3);
+                case GregorianCalendar.MONDAY:
+                    winterEndDateCalendar.add(GregorianCalendar.DATE, -3);
                     break;
             }
 
@@ -169,9 +170,9 @@ public class DateCorrection {
     }
 
     public Date getCorrectStartDate(Date startDate, Date endDate) {
-        Calendar correctStartDateCal = Calendar.getInstance();
+        Calendar correctStartDateCal = GregorianCalendar.getInstance();
 
-        Calendar startDateCal = Calendar.getInstance();
+        Calendar startDateCal = GregorianCalendar.getInstance();
         startDateCal.setTime(startDate);
 
         Date semesterStartDate = thisYearSummerStartDate;
@@ -184,28 +185,28 @@ public class DateCorrection {
             semesterStartDate = lastYearSummerStartDate;
         }
 
-        //Date semesterStartDate = getSemesterStartDate(startDateCal.get(Calendar.YEAR), getSemesterFromStartDate(startDateCal));
+        //Date semesterStartDate = getSemesterStartDate(startDateCal.get(GregorianCalendar.YEAR), getSemesterFromStartDate(startDateCal));
 
         correctStartDateCal.setTime(semesterStartDate);
 
         // zu nächsten Wochentag des startDates setzen
-        while (correctStartDateCal.get(Calendar.DAY_OF_WEEK) != startDateCal.get(Calendar.DAY_OF_WEEK)) {
-            correctStartDateCal.add(Calendar.DATE, 1);
+        while (correctStartDateCal.get(GregorianCalendar.DAY_OF_WEEK) != startDateCal.get(GregorianCalendar.DAY_OF_WEEK)) {
+            correctStartDateCal.add(GregorianCalendar.DATE, 1);
         }
 
         // Zeit richtig setzen
-        correctStartDateCal.set(Calendar.HOUR_OF_DAY, startDateCal.get(Calendar.HOUR_OF_DAY));
-        correctStartDateCal.set(Calendar.MINUTE, startDateCal.get(Calendar.MINUTE));
-        correctStartDateCal.set(Calendar.SECOND, startDateCal.get(Calendar.SECOND));
-        correctStartDateCal.set(Calendar.MILLISECOND, startDateCal.get(Calendar.MILLISECOND));
+        correctStartDateCal.set(GregorianCalendar.HOUR_OF_DAY, startDateCal.get(GregorianCalendar.HOUR_OF_DAY));
+        correctStartDateCal.set(GregorianCalendar.MINUTE, startDateCal.get(GregorianCalendar.MINUTE));
+        correctStartDateCal.set(GregorianCalendar.SECOND, startDateCal.get(GregorianCalendar.SECOND));
+        correctStartDateCal.set(GregorianCalendar.MILLISECOND, startDateCal.get(GregorianCalendar.MILLISECOND));
 
         return correctStartDateCal.getTime();
     }
 
     public Date getCorrectEndDate(Date startDate, Date endDate) {
-        Calendar correctEndDateCal = Calendar.getInstance();
+        Calendar correctEndDateCal = GregorianCalendar.getInstance();
 
-        Calendar endDateCal = Calendar.getInstance();
+        Calendar endDateCal = GregorianCalendar.getInstance();
         endDateCal.setTime(endDate);
 
         Date semesterEndDate = thisYearSummerEndDate;
@@ -218,20 +219,20 @@ public class DateCorrection {
             semesterEndDate = lastYearSummerEndDate;
         }
 
-        //Date semesterEndDate = getSemesterEndDate(endDateCal.get(Calendar.YEAR), getSemesterFromEndDate(endDateCal));
+        //Date semesterEndDate = getSemesterEndDate(endDateCal.get(GregorianCalendar.YEAR), getSemesterFromEndDate(endDateCal));
 
         correctEndDateCal.setTime(semesterEndDate);
 
         // zu vorherigem Wochentag des startDates setzen
-        while (correctEndDateCal.get(Calendar.DAY_OF_WEEK) != endDateCal.get(Calendar.DAY_OF_WEEK)) {
-            correctEndDateCal.add(Calendar.DATE, -1);
+        while (correctEndDateCal.get(GregorianCalendar.DAY_OF_WEEK) != endDateCal.get(GregorianCalendar.DAY_OF_WEEK)) {
+            correctEndDateCal.add(GregorianCalendar.DATE, -1);
         }
 
         // Zeit richtig setzen
-        correctEndDateCal.set(Calendar.HOUR_OF_DAY, endDateCal.get(Calendar.HOUR_OF_DAY));
-        correctEndDateCal.set(Calendar.MINUTE, endDateCal.get(Calendar.MINUTE));
-        correctEndDateCal.set(Calendar.SECOND, endDateCal.get(Calendar.SECOND));
-        correctEndDateCal.set(Calendar.MILLISECOND, endDateCal.get(Calendar.MILLISECOND));
+        correctEndDateCal.set(GregorianCalendar.HOUR_OF_DAY, endDateCal.get(GregorianCalendar.HOUR_OF_DAY));
+        correctEndDateCal.set(GregorianCalendar.MINUTE, endDateCal.get(GregorianCalendar.MINUTE));
+        correctEndDateCal.set(GregorianCalendar.SECOND, endDateCal.get(GregorianCalendar.SECOND));
+        correctEndDateCal.set(GregorianCalendar.MILLISECOND, endDateCal.get(GregorianCalendar.MILLISECOND));
 
         return correctEndDateCal.getTime();
     }
