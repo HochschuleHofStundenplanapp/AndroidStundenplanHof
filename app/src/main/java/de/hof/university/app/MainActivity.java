83e728bd2e797621c997dfa17dd06a7937468f23
NOTEBOOKAV4\stepping <michael.stepping@avinotec.de>
2017-12-23 21:40:47 +0100

    unnecessary import

--- app/src/main/java/de/hof/university/app/MainActivity.java
+++ app/src/main/java/de/hof/university/app/MainActivity.java
@@ -35,7 +35,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
